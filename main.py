6738fc4e1c1ded726fedccce6cc3404fb5562cb3
farihaZ <fsharif@institutmvm.cat>
2021-12-02 15:45:04 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,119 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def main():
+    # n1 = int(input("introduce the inicial value:"))
+    # n2 = int(input("introduce the number2:"))
+    # print("the range between two numbers is")
+    #
+    # for x in range(n1, n2):
+    #     print(x)
+
+    # num = int(input("introduce the month from 1 to 12:"))
+    # while num < 1 or num > 12:
+    #     num = int(input("introduce the month from 1 to 12:"))
+    # match num:
+    #     case 1:
+    #         num = 31
+    #         print("the january has 31 days ")
+    #     case 2:
+    #         num = 28
+    #         print("the february has 28 days ")
+    #     case 3:
+    #         num = 31
+    #         print("the march has 31 days ")
+    #     case 4:
+    #         num = 30
+    #         print("the april has 30 days ")
+    #     case 5:
+    #         num = 31
+    #         print("the may has 31 days ")
+    #     case 6:
+    #         num = 30
+    #         print("the june has 30 days ")
+    #     case 7:
+    #         num = 31
+    #         print("the july has 31 days ")
+    #     case 8:
+    #         num = 31
+    #         print("the august has 31 days ")
+    #     case 9:
+    #         num = 30
+    #         print("the september has 30 days")
+    #     case 10:
+    #         num = 31
+    #         print("the october has 31 days ")
+    #     case 11:
+    #         num = 30
+    #         print("the november has 30 days ")
+    #     case 12:
+    #         num = 31
+    #         print("the december has 31 days ")
+    #     case _:
+    #         print("Error")
+
+
+
+    # n1 = int(input("introduce the first number:"))
+    # n2 = int(input("introduce the second number"))
+    #
+    # aux=n1
+    # n1=n2
+    # n2=aux
+    #
+    # print("the values of n1:", n1, "\nthe value of n2:", n2)
+
+
+
+    # n1, n2 = int(input("introduce the first number:")), int(input("introduce the second number"))
+    # n1, n2 = n2, n1
+    # print("the values of n1:", n1, "\nthe value of n2:", n2)
+
+
+    # num = int(input("introduce the natural number:"))
+    # while num < 0:
+    #     num = int(input("introduce the natural number:"))
+    # if num > 0:
+    #     print("the num is:", num)
+
+      # lista = ['aple', 1, 2.5]
+      # print(lista)
+      #
+      # for x in lista :
+      #     print(x)
+      #
+      # print("---------")
+      #
+      # lista.append(True)
+      # print(lista)
+      #
+      #
+      # lista[3] = False
+      # print(lista)
+      #
+      # lista.insert(2,20)
+      # print(lista)
+      #
+      # lista.remove(False)
+      # print(len(lista))
+      #
+      # del lista[0]
+      # print(len(lista))
+      #
+      # lista.clear()
+      # print(lista)
+
+      # lista = [10,20,30,40,50]
+      # print(lista[-3:-1])
+
+      #print(input("frase:")[::-1])
+      lista =input("introduce the list ")
+      while lista != 0 :
+         print(lista)
+
+
+if __name__ == '__main__':
+    main()
